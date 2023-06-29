e8e4ff93a5d7c95afe7ab73ef210c852d3992e60
Nomthandazo Ndweni <ndweni2658@gmail.com>
2023-06-29 09:19:11 +0200

    Added privacy policy page

---
+++ php-ocls/privacy.php
@@ -0,0 +1,10 @@
+<section class="py-5">
+    <div class="container">
+        <div class="card rounded-0">
+            <div class="card-body">
+                <!--Added privacy policy page-->
+                <?php include "privacy_policy.html" ?>
+            </div>
+        </div>
+    </div>
+</section>
